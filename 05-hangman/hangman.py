dee75c9690a6091d564fa1917867e04060b7ac24
Amanuel <amanuelalemayehuseta@gmail.com>
2026-09-05 19:08:08 +0300

    Get the user input for hangman

--- 05-hangman/hangman.py
+++ 05-hangman/hangman.py
@@ -1,4 +1,33 @@
 import random
 from words import words
+import string
 
-print(words)
+
+def get_valid_word(words):
+    word = random.choice(words)  # randomly chooses something from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)  # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()  # what the user has guessed
+
+
+    # getting user input
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in used_letters:
+            print('\nYou have already used that letter. Guess another letter.')
+
+        else:
+            print('\nThat is not a valid letter.')
+
