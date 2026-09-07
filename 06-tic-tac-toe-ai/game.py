ed8cce43d22b1f73fdeb5b0638d7a3d5017cd66f
Amanuel <amanuelalemayehuseta@gmail.com>
2026-09-07 14:06:05 +0300

    Add available move checking on tic-toc-toe

--- 06-tic-tac-toe-ai/game.py
+++ 06-tic-tac-toe-ai/game.py
@@ -6,8 +6,11 @@ class TicTacToe:
         for row in [self.board[i * 3:(i + 1) * 3] for i in range(3)]:
             print("| " + " | ".join(row) + " |")
 
+    def available_moves(self):
+        return [i for i, square in enumerate(self.board) if square == " "]
+
     def make_move(self, square, letter):
-        if self.board[square] == " ":
+        if square in self.available_moves():
             self.board[square] = letter
             return True
 
@@ -19,4 +22,5 @@ game = TicTacToe()
 game.make_move(4, "X")
 game.make_move(0, "O")
 
+print("Available moves:", game.available_moves())
 game.print_board()
