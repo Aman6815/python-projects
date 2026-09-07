957a8e57917b0345b359c2cabea2117e8886cd30
Amanuel <amanuelalemayehuseta@gmail.com>
2026-09-07 14:14:22 +0300

    Add winner detection on tic-toc-toe

--- 06-tic-tac-toe-ai/game.py
+++ 06-tic-tac-toe-ai/game.py
@@ -16,11 +16,41 @@ class TicTacToe:
 
         return False
 
+    def winner(self, square, letter):
+        row_index = square // 3
+        row = self.board[row_index * 3:(row_index + 1) * 3]
+
+        if all(square == letter for square in row):
+            return True
+
+        column_index = square % 3
+        column = [self.board[column_index + i * 3] for i in range(3)]
+
+        if all(square == letter for square in column):
+            return True
+
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            diagonal2 = [self.board[i] for i in [2, 4, 6]]
+
+            if all(square == letter for square in diagonal1):
+                return True
+
+            if all(square == letter for square in diagonal2):
+                return True
+
+        return False
+
 
 game = TicTacToe()
 
-game.make_move(4, "X")
-game.make_move(0, "O")
+game.make_move(0, "X")
+game.make_move(3, "O")
+game.make_move(1, "X")
+game.make_move(4, "O")
+game.make_move(2, "X")
 
-print("Available moves:", game.available_moves())
 game.print_board()
+
+if game.winner(2, "X"):
+    print("X wins!")
