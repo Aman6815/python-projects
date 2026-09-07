04b0b0d2dbec09e5645fd26bde23fd4b51157dba
Amanuel <amanuelalemayehuseta@gmail.com>
2026-09-07 13:51:24 +0300

    Add player move functionality to tic-toc-toe

--- 06-tic-tac-toe-ai/game.py
+++ 06-tic-tac-toe-ai/game.py
@@ -6,6 +6,17 @@ class TicTacToe:
         for row in [self.board[i * 3:(i + 1) * 3] for i in range(3)]:
             print("| " + " | ".join(row) + " |")
 
+    def make_move(self, square, letter):
+        if self.board[square] == " ":
+            self.board[square] = letter
+            return True
+
+        return False
+
 
 game = TicTacToe()
+
+game.make_move(4, "X")
+game.make_move(0, "O")
+
 game.print_board()
