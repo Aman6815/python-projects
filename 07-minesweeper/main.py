7f37a7ade744c88f7f98b22dabe1434347f4e316
Amanuel <amanuelalemayehuseta@gmail.com>
2026-09-10 03:58:57 +0300

    Add random mine placement

--- 07-minesweeper/main.py
+++ 07-minesweeper/main.py
@@ -1,4 +1,8 @@
+import random
+
+
 BOARD_SIZE = 5
+NUMBER_OF_MINES = 5
 
 
 def create_board():
@@ -15,10 +19,23 @@ def create_board():
     return board
 
 
+def place_mines(board):
+    mines_placed = 0
+
+    while mines_placed < NUMBER_OF_MINES:
+        row = random.randrange(BOARD_SIZE)
+        column = random.randrange(BOARD_SIZE)
+
+        if board[row][column] == "□":
+            board[row][column] = "*"
+            mines_placed += 1
+
+
 def display_board(board):
     for row in board:
         print(" ".join(row))
 
 
 board = create_board()
+place_mines(board)
 display_board(board)
