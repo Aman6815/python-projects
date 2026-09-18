16e3e035cfa96a6f87f1a09b7c3102fba9811fdf
Amanuel <amanuelalemayehuseta@gmail.com>
2026-09-18 04:07:24 +0300

    Return search result index

--- 03-binary-search/main.py
+++ 03-binary-search/main.py
@@ -1,9 +1,9 @@
 def linear_search(numbers, target):
-    for number in numbers:
-        if number == target:
-            return True
+    for index in range(len(numbers)):
+        if numbers[index] == target:
+            return index
 
-    return False
+    return -1
 
 
 def binary_search(numbers, target):
@@ -14,23 +14,23 @@ def binary_search(numbers, target):
         middle = (low + high) // 2
 
         if numbers[middle] == target:
-            return True
+            return middle
 
         if target < numbers[middle]:
             high = middle - 1
         else:
             low = middle + 1
 
-    return False
+    return -1
 
 
 numbers = [3, 7, 12, 18, 25, 31, 42, 56, 68, 79]
 
 target = int(input("Enter a number to search for: "))
 
-found = binary_search(numbers, target)
+index = binary_search(numbers, target)
 
-if found:
-    print(f"{target} was found in the list.")
+if index != -1:
+    print(f"{target} was found at index {index}.")
 else:
     print(f"{target} was not found in the list.")
