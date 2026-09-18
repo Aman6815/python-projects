9b8a3f97e2858c6acfc7f121173401bdc2da5d3a
Amanuel <amanuelalemayehuseta@gmail.com>
2026-09-18 04:01:15 +0300

    Implement binary search algorithm

--- 03-binary-search/main.py
+++ 03-binary-search/main.py
@@ -6,11 +6,29 @@ def linear_search(numbers, target):
     return False
 
 
+def binary_search(numbers, target):
+    low = 0
+    high = len(numbers) - 1
+
+    while low <= high:
+        middle = (low + high) // 2
+
+        if numbers[middle] == target:
+            return True
+
+        if target < numbers[middle]:
+            high = middle - 1
+        else:
+            low = middle + 1
+
+    return False
+
+
 numbers = [3, 7, 12, 18, 25, 31, 42, 56, 68, 79]
 
 target = int(input("Enter a number to search for: "))
 
-found = linear_search(numbers, target)
+found = binary_search(numbers, target)
 
 if found:
     print(f"{target} was found in the list.")
