351586f7fea80655a8cf3c7abb42741e31af043a
Amanuel <amanuelalemayehuseta@gmail.com>
2026-09-20 02:31:18 +0300

    Add automated search tests

---
+++ 03-binary-search/test_search.py
@@ -0,0 +1,25 @@
+from search import binary_search, linear_search
+
+
+def test_linear_search_finds_value():
+    numbers = [3, 7, 12, 18, 25]
+
+    assert linear_search(numbers, 12) == 2
+
+
+def test_linear_search_returns_minus_one_when_missing():
+    numbers = [3, 7, 12, 18, 25]
+
+    assert linear_search(numbers, 10) == -1
+
+
+def test_binary_search_finds_value():
+    numbers = [3, 7, 12, 18, 25]
+
+    assert binary_search(numbers, 18) == 3
+
+
+def test_binary_search_returns_minus_one_when_missing():
+    numbers = [3, 7, 12, 18, 25]
+
+    assert binary_search(numbers, 10) == -1
